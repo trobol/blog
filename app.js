e3e6b9d5597b552732ce22d45d539ce765e4a165
thorfernbacher <thorfernbacher@gmail.com>
2018-12-13 11:10:01 -0500

    s

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express'),
+app = express(),
+http = require('http'),
+fs = require('fs'),
+spdy = require('spdy'),
+posts = require('./posts.js'),
+path = require('path'),
+portHttp = 80,
+portHttps = 443;
+
+
+app.use(function(req, res, next) {
+	if(!path.extname(req.path)) {
+		res.sendFile('public/index.html' , { root : __dirname});
+		console.log("BOOP");
+	} else {
+		console.log(req.path);
+		next();
+	}
+});	
+app.use(posts);
+app.use(express.static('public'));
+
+module.exports = app;
