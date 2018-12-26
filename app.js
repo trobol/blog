c6a29c7be419e68280db33921de5d98171db67d4
Thornton <thorfernbacher@gmail.com>
2018-12-26 17:35:56 -0600

    added pushing

--- app.js
+++ app.js
@@ -1,24 +1,70 @@
 const express = require('express'),
 app = express(),
-http = require('http'),
-fs = require('fs'),
-spdy = require('spdy'),
 posts = require('./posts.js'),
+push = require('./push.js')
 path = require('path'),
-portHttp = 80,
-portHttps = 443;
+fs = require('fs');
 
 
 app.use(function(req, res, next) {
 	if(!path.extname(req.path)) {
-		res.sendFile('public/index.html' , { root : __dirname});
-		console.log("BOOP");
+		let files = {
+			files: [
+				{
+					location:'/style.css',
+					path:'style.css',
+					type: 'text/css'
+				},
+				{
+					location:'/main.js',
+					path:'main.js',
+					type: 'application/javascript'
+				},
+				{
+					location:'/img/spinner.svg',
+					path:'img/spinner.svg',
+					type: 'image/svg+xml'
+				},
+				{
+					location:'/fonts.css',
+					path:'fonts.css',
+					type: 'text/css'
+				},
+				{
+					location:'/fonts/righteous-v6-latin-regular.woff2',
+					path:'fonts/righteous-v6-latin-regular.woff2',
+					type: 'font/woff2'
+				},
+				{
+					location:'/img/background.gif',
+					path:'img/background.gif',
+					type: 'image/gif'
+				},
+				{
+					location:'/favicon.ico',
+					path:'favicon.ico',
+					type: 'image/x-icon'
+				},
+				{
+					location:'/manifest.json',
+					path:'manifest.json',
+					type: 'application/manifest+json'
+				}
+
+			],
+			root:`${__dirname}/public`
+		};
+		push(res, files);
+		res.writeHead(200);
+		res.end(fs.readFileSync(`${__dirname}/public/index.html`));
+		  
 	} else {
 		console.log(req.path);
 		next();
 	}
 });	
-app.use(posts);
-app.use(express.static('public'));
+app.use(posts.router);
+
+app.use(express.static(`${__dirname}/public`));
 
 module.exports = app;
